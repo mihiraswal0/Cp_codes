60830ca2b13e0d07795eddd3f7f828b9fef1aa5e
Mihir Aswl <91006719+mihiraswal0@users.noreply.github.com>
2023-03-23 07:43:43 +0530

    Update binaryindextree.cpp

--- binaryindextree.cpp
+++ binaryindextree.cpp
@@ -1,70 +1,32 @@
-/*
-Question 1 Given array of size N and Q querries
-Solve two type of quereies
-Type1 : i x : replace ith index by value x
-Type 2 : l r : find sum from l to r
-Input :
-N Q
-N elemnts of array
-Type i x / Type l r(Q lines)
-
-
-Example :
-5 4
-3 4 5 6 3
-2 2 4
-1 2 6
-2 2 4
-2 1 5
-Output : 
-15
-17
-23
-
-
-*/
-
-
-#include<bits/stdc++.h>
-using namespace std;
-
-const int N = 1e5+10;
-int bit[N];
-
-void update(int i, int x){
-	for(; i < N; i += (i&-i))
-		bit[i] += x;
-}
-
-int sum(int i){
-	int ans = 0;
-	for(; i > 0; i -= (i&-i))
-		ans += bit[i];
-	return ans;
-}
-
-int main(){
-	int n, q;
-	cin >> n >> q;
-	int a[n+10];
-	for(int i = 1;i <= n; ++i){
-		cin >> a[i];
-		update(i, a[i]);
-	}
-
-	while(q--){
-		int type;
-		cin >> type;
-		if(type == 1){
-			int i, x;
-			cin >> i >> x;
-			update(i, x - a[i]);
-			a[i] = x;
-		}
-		else{
-			int l, r;
-			cin >> l >> r;
-			cout << sum(r) - sum(l-1) << endl;
-		}
-	}
-}
+struct BIT
+{
+    int n;
+    vi bit;
+    BIT(int N)
+    {
+        n = N;
+        bit.assign(n + 1,0);
+    }
+    void update(int i,int v)
+    {
+        for (;i<=n;i += i & (-i))
+            bit[i] = (bit[i] + v)%mod;
+    }
+    int query(int i)
+    {
+        int ret = 0;
+        for (;i>0;i-= i & (-i))ret = (ret + bit[i])%mod;
+        return ret;
+    }
+    
+    void update(int l,int r,int v)
+ 
+    //a[l] += v, a[l + 1] += v, ... a[n] += v,
+    //a[r + 1] -= v,a[r + 2] -= v,a[n] -= v
+    //=> a[l] to a[r] are all increased by +v
+ 
+    {
+        update(l,v);
+        if (r != n)update(r + 1,-v);
+    }
+};
