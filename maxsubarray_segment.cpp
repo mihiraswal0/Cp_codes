94745e49e1c862afb2137304c44798ab743363b0
Mihir Aswl <91006719+mihiraswal0@users.noreply.github.com>
2023-06-17 13:19:26 +0530

    Update maxsubarray_segment.cpp

--- maxsubarray_segment.cpp
+++ maxsubarray_segment.cpp
@@ -1,61 +1,76 @@
-struct DynamicMaxSubarraySum {
-    struct node {
-        ll pref, suf, val, sum;
-    };  
-    int N;
-    ll neutral;
-    vector<node> t;
-    DynamicMaxSubarraySum(int _N, ll assign_value) {
-        neutral = assign_value;
-        N = _N;
-        t.resize(4 * N);
-        forn(i, 4 * N) t[i] = {0, 0, 0, 0};
-        build(1, 0, N - 1); 
-    }
-    void build(int i, int l, int r) {
-        if(l == r) {
-            t[i].pref = t[i].suf = t[i].val = t[i].sum = neutral;
-            return;
-        }
-        int mid = (l + r) >> 1;
-        build(2 * i, l, mid);
-        build(2 * i + 1, mid + 1, r);
-        t[i] = merge(t[2 * i], t[2 * i + 1]);
-    }
-    node merge(node a, node b) {
-        node c;
-        c.pref = max(a.pref, a.sum + b.pref);
-        c.suf = max(b.suf, b.sum + a.suf);
-        c.val = max({a.val, b.val, a.suf + b.pref});
-        c.sum = a.sum + b.sum;
-        return c;
-    }
- 
-    void modif(int i, int l, int r, int pos, ll val) {
-        if(l > pos || r < pos) return;
-        if(l == pos && r == pos) {
-            t[i].pref = t[i].suf = t[i].val = t[i].sum = val;
-            return;
-        }
-        int mid = (l + r) >> 1;
-        modif(2 * i, l, mid, pos, val);
-        modif(2 * i + 1, mid + 1, r, pos, val);
-        t[i] = merge(t[2 * i], t[2 * i + 1]);
-    }
-    node query(int i, int l, int r, int tl, int tr) {
-        if(l > tr || r < tl) return {0, 0, 0, 0};
-        if(l >= tl && r <= tr) return t[i];
-        int mid = (l + r) >> 1;
-        return merge(query(2 * i, l, mid, tl, tr), query(2 * i + 1, mid + 1, r, tl, tr));
-    }
- 
-    void modif(int pos, int val) {
-        modif(1, 0, N - 1, pos, val);
-    }
-    node query(int l, int r) {
-        return query(1, 0, N - 1, l, r);
-    }
-    node query(int pos) {
-        return query(1, 0, N - 1, pos, pos);
-    }
+class node {
+public:
+	ll maxsum, mprefix, msuffix, sum;
+	node()
+	{
+		maxsum = 0;
+		mprefix = 0;
+		msuffix = 0;
+		sum = 0;
+	}
+};
+class SEG {
+public:
+	vector<node> seg;
+	SEG(int n) {
+		seg.resize(4 * n);
+	}
+
+	node combine(node left, node right)
+	{
+		node cur;
+		cur.sum = left.sum + right.sum;
+		cur.mprefix = max(left.mprefix, left.sum + right.mprefix);
+		cur.msuffix = max(right.msuffix, right.sum + left.msuffix);
+		cur.maxsum = max({left.maxsum, right.maxsum, left.msuffix + right.mprefix});
+		return cur;
+
+	}
+	void build(int ind, int low, int high, vector<ll>&arr) {
+		if (low == high) {
+			seg[ind].sum = arr[low];
+			seg[ind].maxsum = max(0ll, arr[low]);
+			seg[ind].mprefix = max(0ll, arr[low]);
+			seg[ind].msuffix = max(0ll, arr[low]);
+			return;
+		}
+
+		int mid = (low + high) / 2;
+		build(2 * ind + 1, low, mid, arr);
+		build(2 * ind + 2, mid + 1, high, arr);
+		seg[ind] = combine(seg[2 * ind + 1], seg[2 * ind + 2]);
+
+	}
+	node query(int ind, int low, int high, int l, int r) {
+		// no overlap
+		// l r low high or low high l r
+		node esi;
+		if (r < low || high < l) return  esi;
+
+		// complete overlap
+		// [l low high r]
+		if (low >= l && high <= r) return seg[ind];
+
+		int mid = (low + high) >> 1;
+		node left = query(2 * ind + 1, low, mid, l, r);
+		node right = query(2 * ind + 2, mid + 1, high, l, r);
+		return combine(left, right);
+	}
+
+	void update(int ind, int low, int high, int i, ll val) {
+		if (low == high) {
+			seg[ind].sum = val;
+			seg[ind].maxsum = max(0ll, val);
+			seg[ind].mprefix = max(0ll, val);
+			seg[ind].msuffix = max(0ll, val);
+			return;
+		}
+
+		int mid = (low + high) >> 1;
+		if (i <= mid) update(2 * ind + 1, low, mid, i, val);
+		else update(2 * ind + 2, mid + 1, high, i, val);
+		seg[ind] = combine(seg[2 * ind + 1] , seg[2 * ind + 2]);
+
+	}
+
 };
